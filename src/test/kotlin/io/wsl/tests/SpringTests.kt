bed43b9b500c268648d1e8513c4f064eeae92116
Kacper Faber <kacperf1234@gmail.com>
2022-09-28 14:49:47 +0200

    To @SpringTests [test] annotation added @SpringBootTest annotation.

--- src/test/kotlin/io/wsl/tests/SpringTests.kt
+++ src/test/kotlin/io/wsl/tests/SpringTests.kt
@@ -1,9 +1,11 @@
 package io.wsl.tests
 
 import io.wsl.WslSpringConfig
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ContextConfiguration
 
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 @ContextConfiguration(classes = [WslSpringConfig::class])
+@SpringBootTest
 annotation class SpringTests()
