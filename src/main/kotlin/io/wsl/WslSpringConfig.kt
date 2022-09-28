7419f6d890846dd4f36ded5c73c833ffb7f03f65
Kacper Faber <kacperf1234@gmail.com>
2022-09-28 13:33:59 +0200

    Created 'WslSpringConfig' which imports the 'io.wsl.*'

---
+++ src/main/kotlin/io/wsl/WslSpringConfig.kt
@@ -0,0 +1,8 @@
+package io.wsl
+
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ComponentScan("io.wsl.*")
+open class WslSpringConfig
