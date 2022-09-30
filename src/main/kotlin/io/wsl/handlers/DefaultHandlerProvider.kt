75bc5643e3b3fcfff546e478ec841f0c6d5dd57c
Kacper Faber <kacperf1234@gmail.com>
2022-09-30 13:47:46 +0200

    Created DefaultHandlerProvider.kt, it has 2 TODO.

--- src/main/kotlin/io/wsl/handlers/DefaultHandlerProvider.kt
+++ src/main/kotlin/io/wsl/handlers/DefaultHandlerProvider.kt
@@ -1,10 +1,18 @@
 package io.wsl.handlers
 
+import io.wsl.extensions.ExtensionModelsProvider
+import io.wsl.reflections.ClassAnnotationsProvider
 import org.springframework.stereotype.Component
 
 @Component
-class DefaultHandlerProvider : HandlerProvider {
+class DefaultHandlerProvider(var classAnnotationsProvider: ClassAnnotationsProvider, var handlerGenerator: HandlerGenerator, var extensionModelsProvider: ExtensionModelsProvider, var handlerIsDefaultChecker: HandlerClassIsDefaultHandlerChecker) : HandlerProvider {
     override fun provide(clazz: Class<*>, handler: io.wsl.Handler): Handler {
-        TODO("Not yet implemented")
+        val isDefault = handlerIsDefaultChecker.check(clazz)
+        val allAnnotations = classAnnotationsProvider.provide(clazz)
+        val extensions = extensionModelsProvider.provide(allAnnotations.toList()) // TODO: replace .toList() with dedicated system
+
+        // TODO: Replace handler.path and handler.allowedOrigins with the dependencies to provide that data.
+
+        return handlerGenerator.generate(handler.path, handler.allowedOrigins, isDefault, clazz, extensions)
     }
 }
