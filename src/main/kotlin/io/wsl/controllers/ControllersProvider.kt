8d62413aecaf2e866b6640170b544d137a553725
Kacper Faber <kacperf1234@gmail.com>
2022-09-30 18:40:18 +0200

    To ControllersProvider.kt [and DefaultControllersProvider.kt] added parameters required by ControllerProvider

--- src/main/kotlin/io/wsl/controllers/ControllersProvider.kt
+++ src/main/kotlin/io/wsl/controllers/ControllersProvider.kt
@@ -1,5 +1,7 @@
 package io.wsl.controllers
 
+import io.wsl.handlers.Handler
+
 interface ControllersProvider {
-    fun provideAll(packagePrefix: String): List<Controller>
+    fun provideAll(packagePrefix: String, handlers: List<Handler>, defaultHandler: Handler?): List<Controller>
 }
--- src/main/kotlin/io/wsl/controllers/DefaultControllersProvider.kt
+++ src/main/kotlin/io/wsl/controllers/DefaultControllersProvider.kt
@@ -1,5 +1,6 @@
 package io.wsl.controllers
 
+import io.wsl.handlers.Handler
 import io.wsl.SocketController
 import io.wsl.listBuilder
 import io.wsl.reflections.ClassAnnotatedWithScanner
@@ -7,11 +8,11 @@ import org.springframework.stereotype.Component
 
 @Component
 class DefaultControllersProvider(var classAnnotatedWithScanner: ClassAnnotatedWithScanner, var controllerProvider: ControllerProvider, var socketControllerAnnotationProvider: SocketControllerAnnotationProvider) : ControllersProvider {
-    override fun provideAll(packagePrefix: String): List<Controller> = listBuilder {
+    override fun provideAll(packagePrefix: String, handlers: List<Handler>, defaultHandler: Handler?): List<Controller> = listBuilder {
         val classList = classAnnotatedWithScanner.scan(packagePrefix, SocketController::class.java)
         for (`class` in classList) {
             val socketController = socketControllerAnnotationProvider.provide(`class`) ?: continue
-            val controller = controllerProvider.provide(`class`, socketController)
+            val controller = controllerProvider.provide(`class`, socketController, handlers, defaultHandler)
             yield(controller)
         }
     }
