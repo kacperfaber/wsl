fe3b95851aebde17856a37b4cd8bea55a6db6811
Kacper Faber <kacperf1234@gmail.com>
2022-10-02 13:26:59 +0200

    Modified Action model - 'clazz' renamed to 'controllerClass' - added parameterList: ParameterList. - deleted TODO.

--- src/main/kotlin/io/wsl/actions/Action.kt
+++ src/main/kotlin/io/wsl/actions/Action.kt
@@ -1,7 +1,7 @@
 package io.wsl.actions
 
 import io.wsl.extensions.ExtensionModel
+import io.wsl.parameters.ParameterList
 import java.lang.reflect.Method
 
-// TODO: Where we put the 'parameters extension'?
-class Action(var method: Method, var clazz: Class<*>, var name: String, var extensions: List<ExtensionModel>)
+class Action(var method: Method, var controllerClass: Class<*>, var name: String, var extensions: List<ExtensionModel>, var parameterList: ParameterList)
