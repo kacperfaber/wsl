e53c58fac1bd5ca2e715bff0f5ece96635e40966
Kacper Faber <kacperf1234@gmail.com>
2022-10-02 14:03:56 +0200

    Created ActionGenerator and ActionGeneratorImpl.

---
+++ src/main/kotlin/io/wsl/actions/ActionGenerator.kt
@@ -0,0 +1,9 @@
+package io.wsl.actions
+
+import io.wsl.extensions.ExtensionModel
+import io.wsl.parameters.ParameterList
+import java.lang.reflect.Method
+
+interface ActionGenerator {
+    fun generate(name: String, method: Method, controllerClass: Class<*>, parameterList: ParameterList, extensions: List<ExtensionModel>): Action
+}
---
+++ src/main/kotlin/io/wsl/actions/ActionGeneratorImpl.kt
@@ -0,0 +1,20 @@
+package io.wsl.actions
+
+import io.wsl.extensions.ExtensionModel
+import io.wsl.parameters.ParameterList
+import org.springframework.stereotype.Component
+import java.lang.reflect.Method
+
+@Component
+class ActionGeneratorImpl : ActionGenerator {
+    override fun generate(
+        name: String,
+        method: Method,
+        controllerClass: Class<*>,
+        parameterList: ParameterList,
+        extensions: List<ExtensionModel>
+    ): Action {
+        return Action(method, controllerClass, name, extensions, parameterList)
+    }
+
+}
