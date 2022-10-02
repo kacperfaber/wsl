e48243b1086f1dc6f7ca7030016d67735ad5f360
Kacper Faber <kacperf1234@gmail.com>
2022-10-02 16:19:21 +0200

    ExtensionModelsProvider.kt - created second method to provide methods with initial list. First method was changed, I created internal method for both of they.

--- src/main/kotlin/io/wsl/extensions/DefaultExtensionModelsProvider.kt
+++ src/main/kotlin/io/wsl/extensions/DefaultExtensionModelsProvider.kt
@@ -1,18 +1,30 @@
 package io.wsl.extensions
 
+import io.wsl.ListBuilder
 import io.wsl.listBuilder
+import io.wsl.listBuilderFrom
 import io.wsl.reflections.AnnotationClassProvider
 import org.springframework.stereotype.Component
 
 @Component
 class DefaultExtensionModelsProvider(var setComponentClassProvider: ExtensionComponentClassProvider, var extensionModelProvider: ExtensionModelProvider, var annotationClassProvider: AnnotationClassProvider, var setComponentProvider: SetComponentAnnotationProvider) : ExtensionModelsProvider {
     override fun provide(annotations: List<Annotation>): List<ExtensionModel>  = listBuilder {
+        provide(annotations, this)
+    }
+
+    override fun provide(annotations: List<Annotation>, initialList: MutableList<ExtensionModel>): MutableList<ExtensionModel> {
+        return listBuilderFrom(initialList) {
+            provide(annotations, this)
+        }
+    }
+
+    internal fun provide(annotations: List<Annotation>, listBuilder: ListBuilder<ExtensionModel>) {
         for (annotation in annotations) {
             val annotationClass = annotationClassProvider.provide(annotation)
             val setComponent = setComponentProvider.provide(annotationClass) ?: continue
             val componentClass = setComponentClassProvider.provide(setComponent)
             val model = extensionModelProvider.provide(annotation, componentClass)
-            yield(model)
+            listBuilder.yield(model)
         }
     }
 }
--- src/main/kotlin/io/wsl/extensions/ExtensionModelsProvider.kt
+++ src/main/kotlin/io/wsl/extensions/ExtensionModelsProvider.kt
@@ -2,4 +2,5 @@ package io.wsl.extensions
 
 interface ExtensionModelsProvider {
     fun provide(annotations: List<Annotation>): List<ExtensionModel>
+    fun provide(annotations: List<Annotation>, initialList: MutableList<ExtensionModel>): MutableList<ExtensionModel>
 }
