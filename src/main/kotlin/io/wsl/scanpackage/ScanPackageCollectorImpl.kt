f4909d4a1c635225532e7e4121d4f40d164b59af
Kacper Faber <kacperf1234@gmail.com>
2022-10-12 15:21:01 +0200

    In ScanPackageCollectorImpl.kt and ScanPrefixCollectorImpl.kt changed dependency - I WAS USING IMPLEMENTATION INSTEAD OF INTERFACE.

--- src/main/kotlin/io/wsl/scanpackage/ScanPackageCollectorImpl.kt
+++ src/main/kotlin/io/wsl/scanpackage/ScanPackageCollectorImpl.kt
@@ -2,13 +2,13 @@ package io.wsl.scanpackage
 
 import io.wsl.ScanPackage
 import io.wsl.listBuilderFrom
-import io.wsl.reflections.SpecifiedAnnotationsByClassFromClassProviderImpl
+import io.wsl.reflections.SpecifiedAnnotationsByClassFromClassProvider
 import org.springframework.stereotype.Component
 
 @Component
-class ScanPackageCollectorImpl(var specifiedAnnotationsByClassFromClassProviderImpl: SpecifiedAnnotationsByClassFromClassProviderImpl) : ScanPackageCollector {
+class ScanPackageCollectorImpl(var specifiedAnnotationsByClassFromClassProvider: SpecifiedAnnotationsByClassFromClassProvider) : ScanPackageCollector {
     override fun collect(clazz: Class<*>, initialList: MutableList<String>): MutableList<String> = listBuilderFrom(initialList) {
-        val scanPackages = specifiedAnnotationsByClassFromClassProviderImpl.provide(clazz, ScanPackage::class.java)
+        val scanPackages = specifiedAnnotationsByClassFromClassProvider.provide(clazz, ScanPackage::class.java)
         for (scanPackage in scanPackages) {
             yieldAll(*scanPackage.packages)
         }
--- src/main/kotlin/io/wsl/scanprefix/ScanPrefixCollectorImpl.kt
+++ src/main/kotlin/io/wsl/scanprefix/ScanPrefixCollectorImpl.kt
@@ -2,13 +2,13 @@ package io.wsl.scanprefix
 
 import io.wsl.ScanPrefix
 import io.wsl.listBuilderFrom
-import io.wsl.reflections.SpecifiedAnnotationsByClassFromClassProviderImpl
+import io.wsl.reflections.SpecifiedAnnotationsByClassFromClassProvider
 import org.springframework.stereotype.Component
 
 @Component
-class ScanPrefixCollectorImpl(var specifiedAnnotationsByClassFromClassProviderImpl: SpecifiedAnnotationsByClassFromClassProviderImpl) : ScanPrefixCollector {
+class ScanPrefixCollectorImpl(var specifiedAnnotationsByClassFromClassProvider: SpecifiedAnnotationsByClassFromClassProvider) : ScanPrefixCollector {
     override fun collect(clazz: Class<*>, initialList: MutableList<String>): MutableList<String> = listBuilderFrom(initialList) {
-        val scanPackages = specifiedAnnotationsByClassFromClassProviderImpl.provide(clazz, ScanPrefix::class.java)
+        val scanPackages = specifiedAnnotationsByClassFromClassProvider.provide(clazz, ScanPrefix::class.java)
         for (scanPackage in scanPackages) {
             yieldAll(*scanPackage.prefixes)
         }
