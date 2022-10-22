f50b5652391d14918247ff9a7fe4832b90a15fae
Kacper Faber <kacperf1234@gmail.com>
2022-10-22 10:39:45 +0200

    ReflectionsForPackageScanProvider - added 'filterInputsBy' and 'setScanners' segment. Before it didn't work.

--- src/main/kotlin/io/wsl/org_reflections/ReflectionsForPackageScanProviderImpl.kt
+++ src/main/kotlin/io/wsl/org_reflections/ReflectionsForPackageScanProviderImpl.kt
@@ -1,12 +1,21 @@
+@file:Suppress("DEPRECATION")
+
 package io.wsl.org_reflections
 
 import org.reflections.Reflections
+import org.reflections.scanners.SubTypesScanner
 import org.reflections.util.ConfigurationBuilder
+import org.reflections.util.FilterBuilder
 import org.springframework.stereotype.Component
 
 @Component
 class ReflectionsForPackageScanProviderImpl : ReflectionsForPackageScanProvider {
     override fun provide(packages: List<String>): Reflections {
-        return Reflections(ConfigurationBuilder().forPackages(*packages.toTypedArray()))
+        val filterBuilder = FilterBuilder()
+        for (`package` in packages) {
+            filterBuilder.includePackage(`package`)
+        }
+
+        return Reflections(ConfigurationBuilder().setScanners(SubTypesScanner(false)).forPackages(*packages.toTypedArray()).filterInputsBy(filterBuilder))
     }
 }
