c299ffd936ef900f0014c2db547162c8fa988586
Kacper Faber <kacperf1234@gmail.com>
2022-11-06 14:03:35 +0100

    SingleActionExecutorImpl, generates actionCall, generates values and provides controllerInstance from spring. Then it will invoke the method.

--- src/main/kotlin/io/wsl/executor/SingleActionExecutorImpl.kt
+++ src/main/kotlin/io/wsl/executor/SingleActionExecutorImpl.kt
@@ -1,12 +1,24 @@
 package io.wsl.executor
 
+import io.wsl.action_call.ActionCallProvider
 import io.wsl.actions.Action
+import io.wsl.reflections.MethodInvoker
 import io.wsl.sessions.Session
 import org.springframework.stereotype.Component
 
 @Component
-class SingleActionExecutorImpl : SingleActionExecutor {
+class SingleActionExecutorImpl(var methodInvoker: MethodInvoker, var actionCallProvider: ActionCallProvider, var controllerInstanceBeanProvider: ControllerInstanceBeanProvider, var parameterValuesCollector: ParameterValuesCollector) : SingleActionExecutor {
     override fun execute(action: Action, session: Session, messageName: String, messageData: String): ExecutionResult {
-        TODO("Not yet implemented")
+        val controllerInstance = controllerInstanceBeanProvider.provide(action.controllerClass)
+        val actionCall = actionCallProvider.provide(controllerInstance, action.method)
+        val values = parameterValuesCollector.collect(action.parameterList, actionCall)
+
+        // TODO: Add PayloadExtension
+
+        val result = methodInvoker.invoke(action.method, controllerInstance, *values)
+
+        // TODO: Add ResultException
+
+        TODO()
     }
 }
