703c52c353e5d512f7a16168db51e39d5a7c2925
Kacper Faber <kacperf1234@gmail.com>
2022-11-07 12:50:22 +0100

    Created ParameterValidator - abstract class extends from 'ExtensionComponent' with single abstract method 'validate(InvokeParameter, ModelState)'

---
+++ src/main/kotlin/io/wsl/extensions/ParameterValidator.kt
@@ -0,0 +1,8 @@
+package io.wsl.extensions
+
+import io.wsl.executor.InvokeParameter
+import io.wsl.model_state.ModelState
+
+abstract class ParameterValidator : ExtensionComponent() {
+    abstract fun validate(invokeParameter: InvokeParameter, modelState: ModelState)
+}
