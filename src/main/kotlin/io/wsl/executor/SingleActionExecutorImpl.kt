9703e7936329bf0c999a29f34bfa5d9715e2ecc1
Kacper Faber <kacperf1234@gmail.com>
2022-11-07 13:53:08 +0100

    Created note in SingleActionExecutorImpl.kt - it works I think.

--- src/main/kotlin/io/wsl/executor/SingleActionExecutorImpl.kt
+++ src/main/kotlin/io/wsl/executor/SingleActionExecutorImpl.kt
@@ -2,27 +2,43 @@ package io.wsl.executor
 
 import io.wsl.action_call.ActionCallProvider
 import io.wsl.actions.Action
+import io.wsl.extensions.ExtensionKind
+import io.wsl.extensions.ExtensionModelsByExtensionKindGrouper
+import io.wsl.extensions.ExtensionModelsByExtensionKindGrouperImpl
 import io.wsl.invoke_parameter.InvokeParameterListProvider
+import io.wsl.invoke_parameter.InvokeParametersValidator
+import io.wsl.model_state.ModelStateGenerator
+import io.wsl.post_extensions_executor.PostExtensionsExecutor
+import io.wsl.pre_extensions_executor.PreExtensionsExecutor
 import io.wsl.reflections.MethodInvoker
 import io.wsl.sessions.Session
 import org.springframework.stereotype.Component
 
 @Component
-class SingleActionExecutorImpl(var valueProvider: ArrayValueProvider, var methodInvoker: MethodInvoker, var actionCallProvider: ActionCallProvider, var controllerInstanceBeanProvider: ControllerInstanceBeanProvider, var invokeParameterListProvider: InvokeParameterListProvider) : SingleActionExecutor {
+class SingleActionExecutorImpl(var preExtensionsExecutor: PreExtensionsExecutor, var extensionsGrouper: ExtensionModelsByExtensionKindGrouper, var postExtensionsExecutor: PostExtensionsExecutor, var modelStateGenerator: ModelStateGenerator, var invokeParametersValidator: InvokeParametersValidator, var valueProvider: ArrayValueProvider, var methodInvoker: MethodInvoker, var actionCallProvider: ActionCallProvider, var controllerInstanceBeanProvider: ControllerInstanceBeanProvider, var invokeParameterListProvider: InvokeParameterListProvider) : SingleActionExecutor {
     override fun execute(action: Action, session: Session, messageName: String, messageData: String): ExecutionResult {
         val controllerInstance = controllerInstanceBeanProvider.provide(action.controllerClass)
         val actionCall = actionCallProvider.provide(controllerInstance, action.method)
         val invokeParameters = invokeParameterListProvider.collect(action.parameterList, actionCall)
+        val modelState = modelStateGenerator.generate()
 
-        // TODO: Make InvokeParameters validated.
+        invokeParametersValidator.doValidation(invokeParameters, modelState)
 
-        // TODO: Add PayloadExtension
+        val extensionsGrouped = extensionsGrouper.group(action.extensions)
+
+        if (extensionsGrouped[ExtensionKind.PreAction] != null) {
+            preExtensionsExecutor.execute(extensionsGrouped[ExtensionKind.PreAction]!!, actionCall)
+        }
 
         val values = valueProvider.provide(invokeParameters)
-        val result = methodInvoker.invoke(action.method, controllerInstance, values) // TODO: No values.
+        val result = methodInvoker.invoke(action.method, controllerInstance, values)
 
-        // TODO: Add ResultException
+        if (extensionsGrouped[ExtensionKind.PostAction] != null) {
+            postExtensionsExecutor.execute(extensionsGrouped[ExtensionKind.PostAction]!!, actionCall, result)
+        }
 
-        TODO()
+        // TODO: I have an idea - Some actions requires no errors, some will be called with errors. If there's more than one action to invoke,
+        // TODO: We can skip to next, when in this ModelState there are errors.
+        return ExecutionResult.CONTINUE
     }
 }
