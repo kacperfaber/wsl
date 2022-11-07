1f32b1afbd5112cc14f30ef1727f217e64ab6c3b
Kacper Faber <kacperf1234@gmail.com>
2022-11-07 11:41:37 +0100

    ParameterValuesCollectorImpl.kt - deals with 'ActionParameter.extensions'

--- src/main/kotlin/io/wsl/executor/ParameterValuesCollectorImpl.kt
+++ src/main/kotlin/io/wsl/executor/ParameterValuesCollectorImpl.kt
@@ -2,20 +2,23 @@ package io.wsl.executor
 
 import io.wsl.action_call.ActionCall
 import io.wsl.extensions.ParameterExtension
+import io.wsl.extensions.SingleActionParameterExtensionProvider
 import io.wsl.parameter_extension_executor.ParameterExtensionExecutor
 import io.wsl.parameters.ParameterList
 import org.springframework.stereotype.Component
 
 @Component
-class ParameterValuesCollectorImpl(var parameterExtensionExecutor: ParameterExtensionExecutor) : ParameterValuesCollector {
+class ParameterValuesCollectorImpl(var singleActionParameterExtensionProvider: SingleActionParameterExtensionProvider, var parameterExtensionExecutor: ParameterExtensionExecutor) : ParameterValuesCollector {
     override fun collect(parameterList: ParameterList, actionCall: ActionCall): Array<Any?> {
         val parametersSize = parameterList.size
         val array = arrayOfNulls<Any?>(parametersSize)
         for (parameterIndex in 0..parametersSize) {
             val parameter = parameterList[parameterIndex]
-            // TODO: Here, we getting value from ParameterExtension. What if they're more than one extension to get the value?
-            if (parameter.extension != null) {
-                val extension = parameter.extension!!
+
+            // TODO: There's a filter, GC deletes the list after firstOrNull be picked.
+            val extension = singleActionParameterExtensionProvider.provide(parameter.extensions)
+
+            if (extension != null) {
                 @Suppress("UNCHECKED_CAST")
                 val componentClass = extension.componentClass as Class<out ParameterExtension>
                 val extensionResult = parameterExtensionExecutor.execute(
