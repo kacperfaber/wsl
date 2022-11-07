9172f8ad11813a6bdc06e5f1367ad9195e6e4006
Kacper Faber <kacperf1234@gmail.com>
2022-11-07 15:06:03 +0100

    Created WslModelBean_BeanTests [not works] and WslModelBean has todo note. Program won't run cause in tests there's no global config class.

--- src/main/kotlin/io/wsl/model/WslModelBean.kt
+++ src/main/kotlin/io/wsl/model/WslModelBean.kt
@@ -3,6 +3,8 @@ package io.wsl.model
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
+// TODO: Cause's exceptions in tests, because in tests there's no GlobalConfig created.
+
 @Configuration
 open class WslModelBean {
     @Bean
