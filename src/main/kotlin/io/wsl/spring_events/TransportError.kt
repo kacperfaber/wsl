cf88c2a6d5d9786837eac617e8e4f5b5fc7b559c
Kacper Faber <kacperf1234@gmail.com>
2022-11-09 15:44:12 +0100

    TransportError.kt - changed 'exception' to 'throwable'

--- src/main/kotlin/io/wsl/spring_events/TransportError.kt
+++ src/main/kotlin/io/wsl/spring_events/TransportError.kt
@@ -2,4 +2,4 @@ package io.wsl.spring_events
 
 import org.springframework.web.socket.WebSocketSession
 
-class TransportError(val session: WebSocketSession, val exception: Exception)
+class TransportError(val session: WebSocketSession, val throwable: Throwable)
