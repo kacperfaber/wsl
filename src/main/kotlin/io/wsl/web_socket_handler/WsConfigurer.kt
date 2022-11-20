395059b25f8da610f95eb6f8f7eb40b18cd3651d
Kacper Faber <kacperf1234@gmail.com>
2022-11-20 15:35:42 +0100

    // TODO: WsConfigurer won't start, unless it's part of the library. When I created the same class in implementation project it will started.

--- src/main/kotlin/io/wsl/web_socket_handler/WsConfigurer.kt
+++ src/main/kotlin/io/wsl/web_socket_handler/WsConfigurer.kt
@@ -5,11 +5,18 @@ import io.wsl.model.GlobalConfigClassBeanRegistered
 import io.wsl.model.WslModel
 import org.springframework.context.annotation.Conditional
 import org.springframework.context.annotation.Configuration
+import org.springframework.stereotype.Component
+import org.springframework.web.socket.config.annotation.EnableWebSocket
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 
 @Configuration
 @Conditional(GlobalConfigClassBeanRegistered::class)
+@EnableWebSocket
+@Component
+
+// TODO: WsConfigurer won't start, unless it's part of the library. When I created the same class in implementation project it will started.
+
 open class WsConfigurer(val actionsByHandlerGrouper: ActionsByHandlerGrouper, val handlerFactory: WebSocketHandlerFactory, val wslModel: WslModel) : WebSocketConfigurer {
     override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
         val actionsGrouped = actionsByHandlerGrouper.group(wslModel.actions, wslModel.controllers, wslModel.handlers)
