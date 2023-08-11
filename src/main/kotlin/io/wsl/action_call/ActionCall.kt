47481652b70257eda138460eaf317332c7082439
Kacper Faber <kacperf1234@gmail.com>
2023-08-11 19:35:54 +0200

    ActionCall - make 'parameters: MutableMap' instead of map.

--- src/main/kotlin/io/wsl/action_call/ActionCall.kt
+++ src/main/kotlin/io/wsl/action_call/ActionCall.kt
@@ -3,7 +3,7 @@ package io.wsl.action_call
 import java.lang.reflect.Method
 
 class ActionCall(
-    var parameters: Map<Class<*>, Any?>,
+    var parameters: MutableMap<Class<*>, Any?>,
     var method: Method,
     var controllerInstance: Any,
     val messageName: String,
